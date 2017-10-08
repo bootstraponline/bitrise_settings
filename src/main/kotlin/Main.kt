4eb052018183cb7701e44df4af6e7ff7167ebdaa
bootstraponline <matt@bootstraponline.com>
2017-10-08 18:13:46 -0400

    Fix click logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Condition.exist
-import com.codeborne.selenide.Condition.visible
-import com.codeborne.selenide.Selenide.getElement
-import com.codeborne.selenide.Selenide.open
+import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.Selenide.*
 import com.codeborne.selenide.SelenideElement
 import org.openqa.selenium.By
 
 // use default 5 second timeout on waitUntil via extension method
 fun SelenideElement.waitUntil(condition: Condition): SelenideElement {
-    return waitUntil(condition, 5000)
+    return waitUntil(condition, 30000)
+}
+
+fun StringBuilder.println(message:String) : StringBuilder {
+    this.append(message + "\n")
+    return this
 }
 
 object Main {
@@ -18,6 +22,7 @@ object Main {
     }
 
     fun find(cssSelector: String): SelenideElement {
+        println("$(\"$cssSelector\")")
         return getElement(By.cssSelector(cssSelector))
     }
 
@@ -44,29 +49,57 @@ object Main {
         System.setProperty("selenide.browser", "Chrome");
     }
 
+    fun click(element: SelenideElement) {
+        return Selenide.executeJavaScript("arguments[0].click()", element.toWebElement()) ?: Unit
+    }
 
-    fun enableAngularInput(selector:String) {
+    fun isAngularInputEnabled(selector: String) : Boolean {
         val input = find(selector).waitUntil(exist)
-        val enabled = input.attr("class").contains("ng-not-empty");
+        val enabled = input.attr("class").contains("ng-not-empty")
+        return enabled
+    }
 
-        if (!enabled) {
-            input.click();
-            find(selector + "[class~='ng-not-empty']").waitUntil(exist)
+    // missing app idle sync from protractor so we have to try a few times in a loop until the event registers.
+    fun enableAngularInput(selector: String) {
+        val enabled = isAngularInputEnabled(selector)
+
+        val tryCount = 10
+        var attempts = 0
+        while (!enabled) {
+            attempts += 1
+            if (attempts > tryCount) throw RuntimeException("Failed to enable input after ${tryCount}x trys")
+
+            click(find(selector))
+            if (find(selector + "[class~='ng-not-empty']").exists()) break
+
+            sleep(500)
         }
     }
 
-    fun disableAngularInput(selector:String) {
-        val input = find(selector).waitUntil(exist)
-        val enabled = input.attr("class").contains("ng-not-empty");
+    fun disableAngularInput(selector: String) {
+        val enabled = isAngularInputEnabled(selector)
 
-        if (!enabled) {
-            input.click();
-            find(selector + "[class~='ng-empty']").waitUntil(exist)
+        val tryCount = 10
+        var attempts = 0
+        while (enabled) {
+            attempts += 1
+            if (attempts > tryCount) throw RuntimeException("Failed to disable input after ${tryCount}x trys")
+
+            click(find(selector))
+            if (find(selector + "[class~='ng-empty']").exists()) break
+
+            sleep(500)
         }
     }
 
-    fun setAppSettings(app:AppObject) {
-       open("https://www.bitrise.io/app/" + app.slug + "#/settings")
+    val report:StringBuilder = StringBuilder()
+
+    fun settingsUrl(app: AppObject) : String {
+        return "https://www.bitrise.io/app/" + app.slug + "#/settings"
+    }
+
+    fun setAppSettings(app: AppObject) {
+        open(settingsUrl(app))
 
         val enableRollingBuilds = "input[ng-model='rollingBuildsAddonCtrl.addonEnabledGetterSetter']"
         enableAngularInput(enableRollingBuilds);
@@ -77,8 +110,14 @@ object Main {
         val pullRequests = checkboxModel + "(1) input"
         val pushes = checkboxModel + "(2) input"
 
-        enableAngularInput(pullRequests);
+        enableAngularInput(pullRequests)
         disableAngularInput(pushes)
+
+        report.println("${app.title} - ${settingsUrl(app)}")
+        report.println("rolling builds? " + isAngularInputEnabled(enableRollingBuilds))
+        report.println("           PRs? " + isAngularInputEnabled(pullRequests))
+        report.println("        pushes? " + isAngularInputEnabled(pushes))
+        report.println("")
     }
 
     @Throws(Exception::class)
@@ -88,8 +127,13 @@ object Main {
 
         goToDashboard()
 
+        var count = 0;
         for (app in Api.getAppsForOrg("instructure")) {
+            count += 1
+            report.append("[$count] ")
             setAppSettings(app)
         }
+
+        println(report.toString())
     }
 }
